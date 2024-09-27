e76ab2052c146601fa3db905e5009541699925dd
niuyiwei <1152472991@qq.com>
2024-09-27 17:13:08 +0800

    :sparkles:完善debugGUI场景，添加控制参数

--- src/scene/debugGUI/debugGUI.js
+++ src/scene/debugGUI/debugGUI.js
@@ -15,7 +15,13 @@ export default class RotateMeshScene {
         this.#renderer = renderer;
     }
     createScene(size) {
-        this.#gui=new GUI({container:document.getElementById('pannel')});
+        //场景参数
+        const params={
+            "color":"#85ca72",
+            "wireframe":false,
+        }
+
+        //scene
         this.#scene = new Three.Scene();
 
         //相机
@@ -28,12 +34,31 @@ export default class RotateMeshScene {
 
         //立方体
         this.#geometry = new Three.BoxGeometry(4, 1, 1);
-        this.#material = new Three.MeshBasicMaterial();
+        this.#material = new Three.MeshBasicMaterial({"color":params.color});
         this.#cube = new Three.Mesh(this.#geometry, this.#material);
 
         this.#scene.add(this.#cube);
 
         this.#renderer.setAnimationLoop(() => this.#animate(this.#cube))
+
+        //gui
+        this.#gui=new GUI({container:document.getElementById('pannel')});
+        this.#gui
+        .addColor(params,'color')
+        .onChange(v=>{
+            this.#material.color.set(v)
+        })
+        this.#gui
+        .add(params,'wireframe')
+        .onChange(v=>{
+            this.#material.wireframe=v;
+        })
+        this.#gui
+        .add(this.#cube.position,'x',-10,10,0.001)
+        this.#gui
+        .add(this.#cube.position,'y',-10,10,0.001)
+        this.#gui
+        .add(this.#cube.position,'z',-10,10,0.001)
     }
     disposeScene() {
         // todo
