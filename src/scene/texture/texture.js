508059fd5b13c230c91cf2cf5fc731ec70ecccdf
niuyiwei <1152472991@qq.com>
2024-10-09 17:10:42 +0800

    :sparkles:textureScene添加纹理切换功能

--- src/scene/texture/texture.js
+++ src/scene/texture/texture.js
@@ -3,6 +3,8 @@ import GUI from "lil-gui";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 export default class TextureScene {
+    #gui;
+
     #renderer;
     #scene;
     camera;
@@ -10,54 +12,74 @@ export default class TextureScene {
     #textureLoader;
 
     #geometry;
-    #material;
+    #leatherArmorTexture;
+    #leatherDiamondPatchesTexture;
+    #leatherArmorMaterial;
+    #leatherDiamondPatchesMaterial;
     #cube;
-    
+
     constructor(renderer) {
         this.#renderer = renderer;
-        this.#textureLoader=new THREE.TextureLoader();
-        const leatherArmorTexture=this.#textureLoader.load('/texture/leatherArmor/Leather_Armor_003_basecolor.png')
-        this.#material=new THREE.MeshBasicMaterial({
-            map:leatherArmorTexture,
+        this.#textureLoader = new THREE.TextureLoader();
+        this.#leatherArmorTexture = this.#textureLoader.load('/texture/leatherArmor/Leather_Armor_003_basecolor.png');
+        this.#leatherDiamondPatchesTexture = this.#textureLoader.load('/texture/leatherDiamondPatches/Leather_Diamond_Patches_002_basecolor.jpg');
+        this.#leatherArmorMaterial = new THREE.MeshBasicMaterial({
+            map: this.#leatherArmorTexture,
+        })
+        this.#leatherDiamondPatchesMaterial = new THREE.MeshBasicMaterial({
+            map: this.#leatherDiamondPatchesTexture
         })
     }
     createScene(size) {
         //场景参数
         const params = {
+            textureType: 'leatherArmor',//type起得不好，之后改一个单词
         }
 
         //scene
-        this.#scene=new THREE.Scene();
-        this.#scene.background=new THREE.Color("#BDC0BA");
+        this.#scene = new THREE.Scene();
+        this.#scene.background = new THREE.Color("#BDC0BA");
 
         //相机
-        this.camera=new THREE.PerspectiveCamera(75,size.width/size.height,0.1,100)
-        this.camera.position.set(0,0,20);
+        this.camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100)
+        this.camera.position.set(0, 0, 20);
 
         //控制器
-        const controls=new OrbitControls(this.camera,this.#renderer.domElement);
+        const controls = new OrbitControls(this.camera, this.#renderer.domElement);
         controls.update();
 
         //球体
-        this.#geometry=new THREE.SphereGeometry( 10, 32, 16 ); 
-        this.#cube=new THREE.Mesh(this.#geometry,this.#material);
+        this.#geometry = new THREE.CylinderGeometry(3, 3, 12, 32);
+        this.#cube = new THREE.Mesh(this.#geometry, this.#leatherArmorMaterial);
 
         this.#scene.add(this.#cube);
 
-        this.#renderer.setAnimationLoop(()=>this.#animate(this.#cube));
+        this.#renderer.setAnimationLoop(() => this.#animate(this.#cube));
+
+        //gui
+        this.#gui = new GUI({ container: document.getElementById('pannel') });
+        //纹理
+        this.#gui
+            .add(params, 'textureType', ['leatherArmor', 'leatherDiamondPatches'])
+            .onChange(value => {
+                console.log(value);
+                if (value === 'leatherDiamondPatches') {
+                    this.#cube.material = this.#leatherDiamondPatchesMaterial;
+                } else if (value === 'leatherArmor') {
+                    this.#cube.material = this.#leatherArmorMaterial;
+                }
+            });
     }
     disposeScene() {
         this.#geometry.dispose();
-        this.#material.dispose();
+        this.#leatherArmorMaterial.dispose();
+        this.#leatherDiamondPatchesMaterial.dispose();
+        this.#gui.destroy();
     }
     #animate(object) {
         // object.rotation.x += 0.01;
         // object.rotation.y += 0.01;
         //渲染器帧渲染场景和相机
-        this.#renderer.render(this.#scene,this.camera);
-    }
-
-    #loadTexture(){
-        //todo
+        this.#renderer.render(this.#scene, this.camera);
     }
 }
