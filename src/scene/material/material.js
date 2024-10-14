3259067ca2b009a3cbd63b4107eda74da09bf303
niuyiwei <1152472991@qq.com>
2024-10-14 11:03:26 +0800

    :sparkles:添加甜甜圈

--- src/scene/material/material.js
+++ src/scene/material/material.js
@@ -4,16 +4,18 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // 这个场景使用requestAnimationFrame来实现动画，以作参考
 // 打算演示几个支持灯光的材质,可能加个卡通什么的，不然写起来有点烦
-// todo，这里测试再加个甜甜圈
+// todo，这里测试再加个甜甜圈,甜甜圈回家找个贴图
 // 基于现在的状况，我需要抽象出
 // 包含gui控制的灯光创建类来简化代码编写
-// 后期可能还会抽象一个scene身上的清理方法，如果用强类型语言，还可以用泛型和多继承整点花活，让代码结构美观点
+// 后期可能还会抽象一个scene身上也抽象一些gui比如fog 的清理方法，如果用强类型语言，还可以用泛型和多继承整点花活，让代码结构美观点
+// 还要抽象一个object综合体：实例化它，会同时添加一个对应的gui folder,包含默认的xyz以及rotation，还有color
 export default class MaterialScene {
     #renderer;
     #scene;
     camera;
 
-    #geometry;
+    #cylinerGeometry;
+    #TorusGeometry;
     #ambientOcclusionTexture;
     #baseColorTexture;
     #heightTexture;
@@ -21,7 +23,10 @@ export default class MaterialScene {
     #normalTexture;
     #roughnessTexture;
     #meshStandardMaterial;
+    #meshToonMaterial;
+
     #cylinder;
+    #torus;
 
     requestAnimationFrameID;
     constructor(render) {
@@ -42,7 +47,9 @@ export default class MaterialScene {
             metalnessMap: this.#metallicTexture,
             roughnessMap: this.#roughnessTexture,
         })
-        // this.#meshStandardMaterial = new THREE.MeshLambertMaterial()
+
+        this.#meshToonMaterial=new THREE.MeshToonMaterial();
+        
     }
     createScene(size) {
 
@@ -52,7 +59,7 @@ export default class MaterialScene {
 
         //相机
         this.camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100);
-        this.camera.position.set(0, 0, 20);
+        this.camera.position.set(0, 0, 40);
 
         //控制器
         const control = new OrbitControls(this.camera, this.#renderer.domElement);
@@ -62,8 +69,8 @@ export default class MaterialScene {
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         this.#scene.add(ambientLight);
 
-        const pointLight = new THREE.PointLight(0xffffff, 50);
-        pointLight.position.set(2, 6, 8);
+        const pointLight = new THREE.PointLight(0xffffff, 100);
+        pointLight.position.set(-5, 8, 8);
         this.#scene.add(pointLight);
 
         const sphereSize = 1;
@@ -72,19 +79,28 @@ export default class MaterialScene {
 
 
         //柱体
-        this.#geometry = new THREE.CylinderGeometry(3, 3, 12, 32);
-        this.#cylinder = new THREE.Mesh(this.#geometry, this.#meshStandardMaterial);
+        this.#cylinerGeometry = new THREE.CylinderGeometry(3, 3, 12, 32);
+        this.#cylinder = new THREE.Mesh(this.#cylinerGeometry, this.#meshStandardMaterial);
 
         this.#scene.add(this.#cylinder)
 
+        //圆环
+        this.#TorusGeometry = new THREE.TorusGeometry(8,3,16,100);
+        this.#torus=new THREE.Mesh(this.#TorusGeometry,this.#meshToonMaterial);
+        this.#torus.position.x=-20;
+
+        this.#scene.add(this.#torus);
+
+        //animation
         const clock = new THREE.Clock();
         this.#animate(this.#cylinder, clock);
-
-
     }
     disposeScene() {
-        this.#geometry.dispose();
+        this.#cylinerGeometry.dispose();
         this.#meshStandardMaterial.dispose();//话说dispose了材质会不会自动清理纹理数据呢，不会，要手动释放
+
+        this.#TorusGeometry.dispose();
+        this.#meshToonMaterial.dispose();
         //todo 清除纹理，封装成一个公共类吧
 
         window.cancelAnimationFrame(this.requestAnimationFrameID);
