7f442d7c13b4bef9be3afde3912f94e90a16c2de
埃罗阿芒老师 <1152472991@qq.com>
2024-10-20 22:15:03 +0800

    :sparkles:添加猎人木屋场景

---
+++ src/scene/hauntedHouse/hauntedHouse.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+import GUI from 'lil-gui';
+
+export default class HauntedHouse{
+  #renderer;
+  #scene;
+  camera;
+
+  constructor(render){
+
+  };
+  createScene(){
+
+  };
+  disposeScene(){
+
+  };
+  #animate(){
+
+  }
+}s
