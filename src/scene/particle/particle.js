456a5c7d476e9f03f10d8b468e8691d8731d5ab8
埃罗阿芒老师 <1152472991@qq.com>
2024-10-25 00:38:32 +0800

    :sparkles:完成粒子动画场景

--- src/scene/particle/particle.js
+++ src/scene/particle/particle.js
@@ -7,14 +7,23 @@ export default class ParticleScene {
     #scene;
     camera;
 
-    gui;
+    #textureLoader;
+
+    #gui;
 
     #boxGeometry;
     #boxMaterial;
     #box;
 
+    #particlesGeometry;
+    #particleMaterial;
+    #particleTexture;
+    #particle;
+
     constructor(render) {
         this.#renderer = render;
+        this.#textureLoader=new THREE.TextureLoader();
+        
     }
 
     createScene(size) {
@@ -35,17 +44,56 @@ export default class ParticleScene {
         this.#box=new THREE.Mesh(this.#boxGeometry,this.#boxMaterial);
         this.#scene.add(this.#box);
 
-        this.#renderer.setAnimationLoop(() => this.#animate())
+        //粒子
+        this.#particlesGeometry=new THREE.BufferGeometry();
+
+        const positions=new Float32Array(10000*3);//这个3如果明白数据是如何传递给gpu的，就很好理解
+        const colors=new Float32Array(10000*3);
+        
+        for (let i = 0; i < 10000*3; i++) {
+            positions[i]=(Math.random()-0.5)*10            
+            colors[i]=Math.random();
+        }
+        this.#particlesGeometry.setAttribute('position',new THREE.BufferAttribute(positions,3))
+        this.#particlesGeometry.setAttribute('color',new THREE.BufferAttribute(colors,3))
+
+        this.#particleTexture=this.#textureLoader.load('/texture/particles/2.png');
+        this.#particleMaterial=new THREE.PointsMaterial();
+        this.#particleMaterial.size=0.1;
+        this.#particleMaterial.sizeAttenuation=true;
+        this.#particleMaterial.color=new THREE.Color('#ff88cc');
+        this.#particleMaterial.transparent=true;
+        this.#particleMaterial.alphaMap=this.#particleTexture;
+        // this.#particleMaterial.alphaTest=0.001;//1
+        // this.#particleMaterial.depthTest=false;//2
+        this.#particleMaterial.depthWrite=false;//3
+        this.#particleMaterial.blending=THREE.AdditiveBlending;
+        this.#particleMaterial.vertexColors=true;
+
+        this.#particle=new THREE.Points(this.#particlesGeometry,this.#particleMaterial);
+        
+        this.#scene.add(this.#particle);
+
+        const clock=new THREE.Clock();
+        this.#renderer.setAnimationLoop(() => this.#animate(clock))
 
         //gui
-        this.gui = new GUI({ container: document.getElementById('pannel') });
+        this.#gui = new GUI({ container: document.getElementById('pannel') });
     }
     disposeScene(){
         console.log('清除');
         this.#boxGeometry.dispose();
         this.#boxMaterial.dispose();
+        this.#gui.destroy();
     }
-    #animate(){
+    #animate(clock){
+        const elapsedTime=clock.getElapsedTime();
+        for (let i = 0; i < 10000; i++) {
+            const i3=i*3;
+            const x=this.#particlesGeometry.attributes.position.array[i3];//这里的数学意义是相位偏移
+            this.#particlesGeometry.attributes.position.array[i3+1]=Math.sin(elapsedTime+x);
+        }
+        this.#particlesGeometry.attributes.position.needsUpdate=true;
         this.#renderer.render(this.#scene, this.camera);
     }
 }
